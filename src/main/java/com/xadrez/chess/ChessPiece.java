84340cbb580e3721051d93d04289b2b330c45971
victorhtl <vht.luna@gmail.com>
2024-02-02 14:14:54 -0300

    Piece movie count

--- src/main/java/com/xadrez/chess/ChessPiece.java
+++ src/main/java/com/xadrez/chess/ChessPiece.java
@@ -5,7 +5,9 @@ import com.xadrez.boardgame.Position;
 import com.xadrez.boardgame.Board;
 
 public abstract class ChessPiece extends Piece {
+
     private Color color;
+    private int moveCount;
 
     public ChessPiece(Board board, Color color){
         super(board);
@@ -14,6 +16,18 @@ public abstract class ChessPiece extends Piece {
 
     public Color getColor(){
         return color;
+    }
+
+    public void increaseMoveCount(){
+        moveCount++;
+    }
+
+    public void decreaseMoveCount(){
+        moveCount--;
+    }
+
+    public int getMoveCount(){
+        return moveCount;
     }
 
     public ChessPosition getChessPosition() {
