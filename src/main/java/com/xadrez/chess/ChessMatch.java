104fb395cf8755fccef920b4c3771ac516093b98
victorhtl <vht.luna@gmail.com>
2024-02-02 15:24:18 -0300

    Queen

--- src/main/java/com/xadrez/chess/ChessMatch.java
+++ src/main/java/com/xadrez/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import com.xadrez.chess.pieces.Bishop;
 import com.xadrez.chess.pieces.King;
 import com.xadrez.chess.pieces.Knight;
 import com.xadrez.chess.pieces.Pawn;
+import com.xadrez.chess.pieces.Queen;
 import com.xadrez.chess.pieces.Rook;
 
 public class ChessMatch {
@@ -229,6 +230,7 @@ public class ChessMatch {
         placeNewPiece('b', 1, new Knight(board, Color.WHITE));
         placeNewPiece('g', 1, new Knight(board, Color.WHITE));
         placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
         placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
         placeNewPiece('e', 1, new King(board, Color.WHITE));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -245,6 +247,7 @@ public class ChessMatch {
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('e', 8, new King(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
